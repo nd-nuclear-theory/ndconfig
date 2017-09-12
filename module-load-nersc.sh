ca3306ac8d7b87e961bd48337ef11ce88c480723
Patrick Fasano <patrick@patrickfasano.com>
2017-09-12 12:49:12 -0700

    module-load-nersc: Add ivybridge load on edison (bash)

--- module-load-nersc.sh
+++ module-load-nersc.sh
@@ -1,4 +1,4 @@
-module load eigen3  # NERSC module is at version 3.3.3 as of 6/19/17 (pjf)
+## module load eigen3  # NERSC module is at version 3.3.3 as of 6/19/17 (pjf)
 ## export EIGEN3_DIR=/global/project/projectdirs/m2032/opt/eigen-3.2.10
 export SPECTRA_DIR=/global/project/projectdirs/m2032/opt/spectra-0.5.0
 module unload cray-libsci
@@ -7,3 +7,10 @@ module load boost
 module load gsl
 module load python/3.5-anaconda
 module load gcc  # needed again as of 6/13/17 (pjf)
+
+# 09/12/2017 (pjf): Fix for edison CRAY_CPU_TARGET by reloading craype-ivybridge
+if [ "$NERSC_HOST" == "edison" ] && [[ -z ${GLOBUS_GRAM_JOB_CONTACT+x} ]]
+then
+  module load craype-ivybridge
+fi
+
