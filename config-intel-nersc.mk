c2ec12e387a606249ca8506abe9783d827d9fec8
Patrick Fasano <patrick@patrickfasano.com>
2017-12-25 21:53:40 -0800

    config: Add additional optimization options at NERSC

--- config-intel-nersc.mk
+++ config-intel-nersc.mk
@@ -4,5 +4,9 @@ include config/ndconfig/config-intel.mk
 FC := ftn
 CXX := CC
 
+CXXFLAGS += -ip
+FFLAGS += -ip
+F90FLAGS += -ip
+
 # keep binaries separate by target architecture
 install_prefix := $(install_prefix)/$(CRAY_CPU_TARGET)
