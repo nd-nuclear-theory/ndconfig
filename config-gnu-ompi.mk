103d1594f7d7220d26490771dfdeae092b575e18
Patrick Fasano <patrick@patrickfasano.com>
2019-03-07 15:24:49 -0800

    config: Add MPI compiler configurations for Intel and GNU

---
+++ config-gnu-ompi.mk
@@ -0,0 +1,5 @@
+include config/ndconfig/config-intel.mk
+
+# use OpenMPI compiler wrappers
+FC := mpifort
+CXX := mpicxx
---
+++ config-intel-impi.mk
@@ -0,0 +1,5 @@
+include config/ndconfig/config-intel.mk
+
+# use Intel MPI compiler wrappers
+FC := mpiifort
+CXX := mpiicpc
