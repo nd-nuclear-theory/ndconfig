d264d83883a70740b0f0dd27db1052e96b5e7f44
Patrick Fasano <patrick@patrickfasano.com>
2021-09-29 22:48:43 -0400

    env: Fix ordering of module loads

--- env-gnu-nersc.sh
+++ env-gnu-nersc.sh
@@ -1,14 +1,13 @@
 export MODULEPATH=/global/common/software/m2032/shared/modulefiles:$MODULEPATH
 
-module load eigen3
-export SPECTRA_DIR=/global/project/projectdirs/m2032/opt/spectra
-
 module -s swap PrgEnv-intel PrgEnv-gnu
+module switch gcc/9.3.0
 module unload cray-libsci
 module load craype-hugepages2M
+
+module load eigen3
+module load spectra
 module load boost
 module load gsl
 module load python
 module load parallel
-## module swap intel/18.0.0.128 intel/17.0.3.191 # 12/21/17 (pjf): intel version 18 has openmp bug
-## module load gcc  # needed again as of 6/13/17 (pjf)
--- env-intel-nersc.sh
+++ env-intel-nersc.sh
@@ -1,16 +1,6 @@
 export MODULEPATH=/global/common/software/m2032/shared/modulefiles:$MODULEPATH
 
-module load eigen3
-
-export SPECTRA_DIR=/global/project/projectdirs/m2032/opt/spectra
-
 module -s swap PrgEnv-gnu PrgEnv-intel
-module unload cray-libsci
-module load craype-hugepages2M
-module load boost
-module load gsl
-module load python
-module load parallel
 
 # The Intel C++ compiler has a bug with redeclaring a static variable as constexpr
 # in versions before 19.1.
@@ -18,5 +8,14 @@ module -s swap intel/19.1.2.254
 
 # Even if using the intel compiler suite, loading GCC is still required, since gcc
 # is used internally by icpc.
+module load gcc/9.3.0  # needed again as of 6/13/17 (pjf)
 
-module load gcc  # needed again as of 6/13/17 (pjf)
+module unload cray-libsci
+module load craype-hugepages2M
+
+module load eigen3
+module load spectra
+module load boost
+module load gsl
+module load python
+module load parallel
