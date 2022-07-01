c2c776970ed02cb54a376ac87b76ac53828bb75b
Patrick Fasano <patrick@patrickfasano.com>
2022-07-01 02:20:09 -0400

    env: Add AOCC environment at NERSC

---
+++ env-aocc-nersc.sh
@@ -0,0 +1,16 @@
+if [[ "$NERSC_HOST" == "perlmutter" ]]; then
+  module load PrgEnv-aocc
+  # TODO: use Spack to generate module on Perlmutter
+  export EIGEN_DIR=/global/common/software/m2032/shared/common/eigen3/3.4.0
+fi
+
+if [[ "$NERSC_HOST" == "cori" ]]; then
+  exit 1
+fi
+
+module load spectra
+module load boost
+module load gsl
+module load python
+module load parallel
+module load cmake
